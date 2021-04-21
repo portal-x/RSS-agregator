5c893921c5286a412017a9953f5f59a68836ee07
Yurii Tkachuk <papirus.borzna@gmail.com>
2021-04-21 18:08:25 +0300

    add watchers

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { watchForm } from './watchers';
 
 export default () => {
-  console.log('hi from webpack!');
+  const state = {
+    urls: [],
+  };
+  const watchedState = watchForm(state);
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    watchedState.urls.push(formData.get('url'));
+  });
+  console.log('state:', state);
 };
